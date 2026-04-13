c29afd5c1b22138bfd10b85f29106ec032064fc1
Neel123priyansh <neelpriyansh@gmail.com>
2026-04-13 21:51:03 +0530

    Cursed with knowledge

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLK7hiur5aLmXxI7WqdW9iAWKBADbNzxo",
-  authDomain: "vitalguard-database.firebaseapp.com",
-  databaseURL: "https://vitalguard-database-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "vitalguard-database",
-  storageBucket: "vitalguard-database.firebasestorage.app",
-  messagingSenderId: "777270337061",
-  appId: "1:777270337061:web:f74cae31b5e6547d00a9cc",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
